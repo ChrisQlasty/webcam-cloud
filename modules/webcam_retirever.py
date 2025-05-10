b058c37ad752a3be6e303fff7fcabb1e426d2682
Krzysztof Czuszynski <qlasty@hotmail.com>
2025-05-10 14:55:10 +0200

    get frame from webcam from url

---
+++ modules/webcam_retirever.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import subprocess
+
+logging.basicConfig(level=logging.INFO)
+
+OUTPUT_FILENAME = "data/frame.jpg"
+STREAM_URL = os.getenv("ENV_STREAM_URL")
+
+
+def main():
+    command = ["ffmpeg", "-y", "-i", STREAM_URL, "-vframes", "1", OUTPUT_FILENAME]
+
+    try:
+        subprocess.run(command, check=True)
+        logging.info(f"Frame extracted successfully to {OUTPUT_FILENAME}")
+    except subprocess.CalledProcessError as e:
+        logging.info(f"Error extracting frame: {e}")
+
+
+if __name__ == "__main__":
+    main()

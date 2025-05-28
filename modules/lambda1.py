3cc8ae6365a6abe34aed055d181ce20976ec17d8
Krzysztof Czuszynski <qlasty@hotmail.com>
2025-05-28 22:45:15 +0200

    update lambda1 for investion code

--- modules/lambda1.py
+++ modules/lambda1.py
@@ -1,8 +1,13 @@
 import json
+import logging
 import os
 import uuid
 
 import boto3
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 dynamodb = boto3.resource("dynamodb")
 lambda_client = boto3.client("lambda")
@@ -12,17 +17,19 @@ TABLE_NAME = os.getenv("TF_VAR_db_table")
 BATCH_RUNNER_NAME = os.getenv("TF_VAR_lambda2")
 SOURCE_BUCKET = os.getenv("TF_VAR_input_bucket")
 DEST_BUCKET = os.getenv("TF_VAR_processed_bucket")
+MODEL_NAME = os.getenv("TF_VAR_obj_det_model")
+INSTANCE_TYPE = os.getenv("instance_type", "ml.m5.large")
 
 table = dynamodb.Table(TABLE_NAME)
 
 
-def call_j():
+def call_batch_transform_job():
     sagemaker = boto3.client("sagemaker")
 
     job_name = f"object-detection-job-{uuid.uuid4().hex[:8]}"
     sagemaker.create_transform_job(
         TransformJobName=job_name,
-        ModelName="object-detection-model",
+        ModelName=MODEL_NAME,
         TransformInput={
             "DataSource": {
                 "S3DataSource": {
@@ -36,43 +43,40 @@ def call_j():
             "S3OutputPath": f"s3://{DEST_BUCKET}/",
             "Accept": "application/json",
         },
-        TransformResources={"InstanceType": "ml.m5.large", "InstanceCount": 1},
+        TransformResources={"InstanceType": INSTANCE_TYPE, "InstanceCount": 1},
     )
 
     return {"status": "Transform job started", "job_name": job_name}
 
 
 def lambda_handler(event, context):
-    print(event)
-    for record in event.get("Records", []):
-        print(record["body"])
-        s3_event = json.loads(record["body"])
-        s3_key = s3_event["Records"][0]["s3"]["object"]["key"]
-        print(f"Adding key to batch: {s3_key}")
+    try:
+        for record in event.get("Records", []):
+            s3_event = json.loads(record["body"])
+            s3_key = s3_event["Records"][0]["s3"]["object"]["key"]
+            logger.info(f"Adding key to batch: {s3_key}")
 
-        # Append key to the batch (atomic update)
-        response = table.update_item(
-            Key={"batch_id": "current"},
-            UpdateExpression="SET images = list_append(if_not_exists(images, :empty), :new)",
-            ExpressionAttributeValues={":new": [s3_key], ":empty": []},
-            ReturnValues="ALL_NEW",
-        )
+            # Append key to the batch (atomic update)
+            response = table.update_item(
+                Key={"batch_id": "current"},
+                UpdateExpression="SET images = list_append(if_not_exists(images, :empty), :new)",
+                ExpressionAttributeValues={":new": [s3_key], ":empty": []},
+                ReturnValues="ALL_NEW",
+            )
 
-        images = response["Attributes"]["images"]
-        print(f"Current batch: {images}")
+            images = response["Attributes"]["images"]
+            logger.info(f"Current batch: {images}")
 
-        # If batch is full, invoke next Lambda and reset
-        if len(images) == BATCH_SIZE:
-            print("Batch full.")
-            # print("Batch full. Invoking batch runner...")
-            # lambda_client.invoke(
-            #     FunctionName=BATCH_RUNNER_NAME,
-            #     InvocationType="Event",  # async
-            #     Payload=json.dumps({"batch": images}),
-            # )
+            # If batch is full, invoke next Lambda and reset
+            if len(images) == BATCH_SIZE:
+                logger.info("Batch full.")
 
-            # Reset batch
-            table.put_item(Item={"batch_id": "current", "images": []})
-            return call_j()
+                # Reset batch
+                table.put_item(Item={"batch_id": "current", "images": []})
+                # TODO: trigger another Lambda when job is finished
+                return call_batch_transform_job()
+    except Exception as e:
+        print(f"Error processing event: {e}")
+        return {"statusCode": 500, "body": str(e)}
 
     return {"statusCode": 200}

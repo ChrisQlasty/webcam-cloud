2ba9746e762e4161614f5d2672df44355a912f01
Krzysztof Czuszynski <qlasty@hotmail.com>
2025-06-20 22:35:59 +0200

    add elastic ip; add scheduler + lambdas for auto ec2 launch

--- terraform/main.tf
+++ terraform/main.tf
@@ -233,14 +233,13 @@ resource "aws_iam_role_policy" "lambda2_policy" {
 # Lambda 1: triggered by SQS
 locals {
   lambda1_zip_path = "${path.module}/../cloud_resources/lambda1.zip"
-  lambda2_zip_path = "${path.module}/../cloud_resources/lambda2.zip"
 }
 resource "aws_lambda_function" "lambda1" {
   filename         = local.lambda1_zip_path
   function_name    = var.lambda1
   role             = aws_iam_role.lambda1_exec_role.arn
   handler          = "modules.lambda1.lambda_handler"
-  runtime          = "python3.11"
+  runtime          = "python3.10"
   timeout          = 20
   source_code_hash = filebase64sha256(local.lambda1_zip_path)
   environment {
@@ -538,3 +537,181 @@ resource "aws_instance" "dash_instance" {
   }
 }
 
+# Allocate a new Elastic IP
+resource "aws_eip" "dash_eip" {
+  tags = {
+    Name = "dash-app-eip"
+  }
+}
+
+# Associate the Elastic IP with our EC2 instance
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.dash_instance.id
+  allocation_id = aws_eip.dash_eip.id
+}
+
+# scheduling EC2 instance start/stop using Lambda and EventBridge
+# 1. IAM Role and Policy for Lambda to manage EC2
+resource "aws_iam_role" "lambda_ec2_scheduler_role" {
+  name = "lambda_ec2_scheduler_role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_ec2_scheduler_policy" {
+  name        = "lambda_ec2_scheduler_policy"
+  description = "Allows Lambda to start/stop a specific EC2 instance and write logs."
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["ec2:StartInstances", "ec2:StopInstances"],
+        Resource = aws_instance.dash_instance.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ec2_scheduler_attachment" {
+  role       = aws_iam_role.lambda_ec2_scheduler_role.name
+  policy_arn = aws_iam_policy.lambda_ec2_scheduler_policy.arn
+}
+
+# 2. Package Lambda functions from local files
+data "archive_file" "start_instance_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../modules/start_ec2.py"
+  output_path = "${path.module}/../cloud_resources/start_ec2.zip"
+}
+
+data "archive_file" "stop_instance_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../modules/stop_ec2.py"
+  output_path = "${path.module}/../cloud_resources/stop_ec2.zip"
+}
+
+# 3. Define the Lambda Functions
+resource "aws_lambda_function" "start_instance_lambda" {
+  function_name = "start-dash-instance"
+  filename      = data.archive_file.start_instance_zip.output_path
+  handler       = "start_ec2.handler"
+  runtime       = "python3.10"
+  role          = aws_iam_role.lambda_ec2_scheduler_role.arn
+  source_code_hash = data.archive_file.start_instance_zip.output_base64sha256
+
+  environment {
+    variables = {
+      INSTANCE_ID = aws_instance.dash_instance.id
+      REGION      = var.region
+    }
+  }
+}
+
+resource "aws_lambda_function" "stop_instance_lambda" {
+  function_name = "stop-dash-instance"
+  filename      = data.archive_file.stop_instance_zip.output_path
+  handler       = "stop_ec2.handler"
+  runtime       = "python3.10"
+  role          = aws_iam_role.lambda_ec2_scheduler_role.arn
+  source_code_hash = data.archive_file.stop_instance_zip.output_base64sha256
+
+  environment {
+    variables = {
+      INSTANCE_ID = aws_instance.dash_instance.id
+      REGION      = var.region
+    }
+  }
+}
+
+# 4. Create EventBridge Schedules to trigger the Lambdas
+# Note: cron expressions are in UTC. "0 10 * * ? *" is 10:00 AM UTC.
+
+resource "aws_scheduler_schedule" "start_ec2_daily" {
+  name       = "start-dash-instance-daily-10am"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(0 10 ? * MON-FRI *)"
+  schedule_expression_timezone = "Europe/Berlin"
+  
+  # The schedule will cease to run after this date.
+  end_date = "2025-07-31T23:59:59Z"
+
+  target {
+    arn      = aws_lambda_function.start_instance_lambda.arn
+    role_arn = aws_iam_role.scheduler_invoke_lambda_role.arn
+  }
+}
+
+resource "aws_scheduler_schedule" "stop_ec2_daily" {
+  name       = "stop-dash-instance-daily-4pm"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression          = "cron(0 16 ? * MON-FRI *)" # 10 AM + 6 hours = 4 PM (16:00)
+  schedule_expression_timezone = "Europe/Berlin"
+
+  end_date = "2025-07-31T23:59:59Z"
+
+  target {
+    arn      = aws_lambda_function.stop_instance_lambda.arn
+    role_arn = aws_iam_role.scheduler_invoke_lambda_role.arn
+  }
+}
+
+# 5. IAM Role for the Scheduler to invoke Lambda
+resource "aws_iam_role" "scheduler_invoke_lambda_role" {
+  name = "scheduler_invoke_lambda_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "scheduler.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "scheduler_invoke_lambda_policy" {
+  name = "scheduler_invoke_lambda_policy"
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "lambda:InvokeFunction"
+      Resource = [
+        aws_lambda_function.start_instance_lambda.arn,
+        aws_lambda_function.stop_instance_lambda.arn,
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler_invoke_lambda_attachment" {
+  role       = aws_iam_role.scheduler_invoke_lambda_role.name
+  policy_arn = aws_iam_policy.scheduler_invoke_lambda_policy.arn
+}
